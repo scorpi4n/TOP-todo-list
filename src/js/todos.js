c658af88ef7316b38290c758d555045159df93d6
James Phillips <scorpi4n@scorpi4n.com>
2023-01-10 01:39:35 -0600

    Add functions for CRUD operations as methods to Todo class

--- src/js/todos.js
+++ src/js/todos.js
@@ -1,37 +1,48 @@
 export class Todo {
-  constructor(title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority, id = crypto.randomUUID()) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.id = id;
   }
 
   display() {
-    console.log(this.title);
-  }
-}
-
-export function saveTodos(todos) {
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-export function getTodos() {
-  let todos = JSON.parse(localStorage.getItem("todos"));
-
-  if (todos === null) {
-    saveTodos([]);
+    console.log(`Title: ${this.title}\nID: ${this.id}`);
   }
 
-  todos.forEach(todo => {
-    let tmp = new Todo(
-      todo.title,
-      todo.description,
-      todo.dueDate,
-      todo.priority
-    );
-    // replace each todo with Todo object to append methods
-    todos[todos.indexOf(todo)] = tmp;
-  });
+  delete() {
+    let todos = loadTodos();
+    todos = todos.filter(function (todo) {
+      return todo.id !== this.id ? true : false;
+    });
+    saveTodos(todos);
+  }
 
-  return todos;
+  static save(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  static load() {
+    let todos = JSON.parse(localStorage.getItem("todos"));
+
+    if (todos === null) {
+      this.save([]);
+      return [];
+    }
+
+    todos.forEach(todo => {
+      let tmp = new Todo(
+        todo.title,
+        todo.description,
+        todo.dueDate,
+        todo.priority,
+        todo.id
+      );
+      // replace each todo with Todo object to append methods
+      todos[todos.indexOf(todo)] = tmp;
+    });
+
+    return todos;
+  }
 }
