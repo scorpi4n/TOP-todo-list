6bebd1447aa52eff5948b91a4a6999f167c8724f
James Phillips <scorpi4n@scorpi4n.com>
2023-01-26 15:03:00 -0600

    Rewrite Task tests

--- tests/Task.test.js
+++ tests/Task.test.js
@@ -1,66 +1,64 @@
-import Task from "../src/js/Task";
+import Task from "../src/modules/Task";
 
-describe("test Task methods", () => {
-  // setup and teardown
-  let tasks;
-  beforeEach(() => {
-    tasks = [
-      new Task("name1", "desc", "personal", new Date(), 1),
-      new Task("name2", "description", "personal", new Date(), 2),
-      new Task("name3", "described", "personal", new Date(), 3),
-      new Task(
-        "name4",
-        "make video: 'how to eat donuts'",
-        "personal",
-        new Date(),
-        4
-      ),
-      new Task("name5", "getting lazy", "personal", new Date(), 5),
-      new Task("name6", "example desc", "personal", new Date(), 1),
-    ];
+// setup and teardown
+let testTasks;
+let returnValue;
+beforeEach(() => {
+  testTasks = [
+    new Task("name1", "desc", new Date(), 1),
+    new Task("name2", "description", new Date(), 2),
+    new Task("name3", "described", new Date(), 3),
+    new Task("name4", "make video: 'how to eat donuts'", new Date(), 4),
+    new Task("name5", "getting lazy", new Date(), 5),
+    new Task("name6", "example desc", new Date(), 1),
+  ];
+});
 
-    Task.save(tasks);
-  });
-
-  afterEach(() => {
-    localStorage.clear();
-  });
-
-  // create
-  test("create static method return value", () => {
-    const result = new Task(
-      "eat dinner",
-      "eat with family",
-      "personal",
-      new Date(),
-      5
-    );
-    expect(result instanceof Task).toBeTruthy();
-  });
-
-  // read
-  test("Task.load returns array", () => {
-    expect(Task.load()).toBeInstanceOf(Array);
-    localStorage.clear();
-    expect(Task.load()).toBeInstanceOf(Array);
-  });
-
-  // update
-  it("should save an array of Tasks", () => {
-    Task.save(tasks);
-    expect(Task.load()).toEqual(tasks);
-  });
-
+describe(".complete instance method", () => {
   it("should set isCompleted to true", () => {
-    tasks.at(3).complete();
-    Task.save(tasks);
-    expect(Task.load().at(3).isCompleted).toBeTruthy();
+    testTasks.forEach(task => {
+      expect(task.isCompleted).toBeFalsy();
+      task.complete();
+      expect(task.isCompleted).toBeTruthy();
+    });
+  });
+});
+
+describe(".delete instance method", () => {
+  it("should return the modified array", () => {
+    expect(testTasks.at(3).delete(testTasks)).toBeInstanceOf(Array);
   });
 
-  // delete
-  it("should remove the task", () => {
-    Task.load().at(2).delete();
-    tasks.splice(2, 1);
-    expect(Task.load()).toEqual(tasks);
+  it("should remove the task which calls it", () => {
+    expect(testTasks.at(3).delete(testTasks).length).toBe(5);
+  });
+});
+
+describe(".save static method", () => {
+  it("should save a JSON array of Tasks", () => {
+    Task.save(testTasks);
+    const savedValue = JSON.parse(localStorage.getItem("tasks"));
+
+    expect(savedValue).toBeInstanceOf(Array);
+  });
+
+  it("should return the array that is passed in as an argument", () => {
+    expect(Task.save(testTasks)).toEqual(testTasks);
+  });
+});
+
+describe(".load static method", () => {
+  beforeEach(() => {
+    Task.save(testTasks);
+  });
+
+  it("should return an array", () => {
+    expect(Task.load()).toBeInstanceOf(Array);
+  });
+
+  test("each value in returned array is a Task", () => {
+    Task.load().forEach(task => {
+      expect(task).toBeInstanceOf(Task);
+    });
   });
 });
