12a4a1457b70d50c2365d5a66cb1c69c2004b320
James Phillips <scorpi4n@scorpi4n.com>
2023-02-05 23:42:03 -0600

    Add eventListener to complete tasks

--- src/components/task.js
+++ src/components/task.js
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
-import { button, createElementWithClass, td } from "../modules/domHelpers";
+import { button, createElementWithClass, td } from "../js/domHelpers";
+import Task from "../js/Task";
 
 export default function (name, dueDate) {
   const nameEl = createElementWithClass("th", "task-name");
@@ -8,6 +9,7 @@ export default function (name, dueDate) {
   const dueDateEl = td(`Due: ${format(dueDate, "P")}`);
 
   const btnEl = button("Complete");
+  btnEl.addEventListener("click", e => handleClick(name, e));
 
   const btnWrapperEl = td();
   btnWrapperEl.appendChild(btnEl);
@@ -16,4 +18,15 @@ export default function (name, dueDate) {
   taskEl.appendChildren(nameEl, dueDateEl, btnWrapperEl);
 
   return taskEl;
+}
+
+function handleClick(taskName, event) {
+  let tasks = Task.load().map(task => {
+    if (taskName === task.name) task.complete();
+    return task;
+  });
+  Task.save(tasks);
+
+  // delete task from visible table
+  event.target.parentElement.parentElement.style.display = "none";
 }
