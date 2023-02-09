cca239f4a0e8fbfc3a3a8cb1c0e8ec5709f31330
James Phillips <scorpi4n@scorpi4n.com>
2023-02-09 16:04:54 -0600

    Delete unused components

--- src/components/header.js
+++
@@ -1,8 +0,0 @@
-export default function () {
-  const headerEl = document.createElement("header");
-
-  const navEl = document.createElement("nav");
-  headerEl.appendChild(navEl);
-
-  return headerEl;
-}
