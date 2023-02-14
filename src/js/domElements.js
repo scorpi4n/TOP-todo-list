f569a029bbad69aa676154d3b8dc0397dc7ba40a
James Phillips <scorpi4n@scorpi4n.com>
2023-02-13 20:07:26 -0600

    Export modal selector

--- src/js/domElements.js
+++ src/js/domElements.js
@@ -2,3 +2,4 @@ export const navbar = document.querySelector("nav");
 export const tasks = document.querySelector(".tasks");
 export const filters = document.querySelector(".filters");
 export const newTaskBtn = document.querySelector("#new-task");
+export const newTaskModal = document.querySelector("#example-modal");
