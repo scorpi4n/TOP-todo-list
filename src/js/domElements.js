ee818e2666f71b1e147db01ead0e336b77e49c13
James Phillips <scorpi4n@scorpi4n.com>
2023-02-14 14:12:38 -0600

    Export form selector

--- src/js/domElements.js
+++ src/js/domElements.js
@@ -3,3 +3,4 @@ export const tasks = document.querySelector(".tasks");
 export const filters = document.querySelector(".filters");
 export const newTaskBtn = document.querySelector("#new-task");
 export const newTaskModal = document.querySelector("#example-modal");
+export const newTaskForm = document.querySelector("#task-form");
